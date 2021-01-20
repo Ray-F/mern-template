3bf4d6c551f2de9379a733f40eddc36508e1e6cc
Raymond Feng <rf.raymondfeng@gmail.com>
2021-01-20 18:59:26 +1300

    [Master] Update syntax to ES2015 on server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+// const mongoClient = require('./models/mongoConnection');
+
+const app = express();
 
 // view engine setup
 app.set('view engine', 'pug')
