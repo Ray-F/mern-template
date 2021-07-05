d5c77cc72ee13c9506c60c51c5dc70083a4cd4f9
Raymond Feng <rf.raymondfeng@gmail.com>
2021-07-05 17:24:20 +1200

    Clean up server www.js

--- server/src/bin/www.js
+++ server/src/bin/www.js
@@ -1,60 +1,38 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
+import app from '../app';
+import debug from 'debug';
+import http from 'http';
 
-require('../utils/Config');
-const app = require('../app');
-const debug = require('debug');
-const http = require('http');
+import Config from '../utils/Config';
 
-/**
- * Get port from environment and store in Express.
- */
 
-const port = normalizePort(process.env.PORT || 9002);
+// Get the port
+const port = normalizePort(Config.PORT);
 console.log('[Server] Hosted on port:', port);
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
 
+// Create HTTP server
 const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
-/**
- * Normalize a port into a number, string, or false.
- */
 
+/**
+ * Normalizes a port into a number, string, or false.
+ */
 function normalizePort(val) {
   const port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
+  return isNaN(port) ? val : port >= 0 ? port : false;
 }
+
 
 /**
  * Event listener for HTTP server "error" event.
  */
-
 function onError(error) {
   if (error.syscall !== 'listen') {
     throw error;
@@ -80,7 +58,6 @@ function onError(error) {
 /**
  * Event listener for HTTP server "listening" event.
  */
-
 function onListening() {
   const addr = server.address();
   const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
--- server/src/utils/Config.js
+++ server/src/utils/Config.js
@@ -4,6 +4,12 @@ dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
+/*
+ * The default port of the application
+ */
+const PORT = process.env.PORT || 9002;
+
 export default {
+  PORT,
   MONGODB_URI,
 };
